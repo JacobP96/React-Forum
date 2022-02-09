26099721282dc1253d3365b294f63a661846bb7b
Jacob Persson <jacobpersson96@gmail.com>
2022-02-09 17:05:07 +0100

    commit

--- my-app/src/components/ShowData.jsx
+++ my-app/src/components/ShowData.jsx
@@ -10,9 +10,6 @@ const ShowData = (props) =>{
           <div className="InfoText">
             <h1 className="PostCommentHeader">Post and comment information about # {props.InfoPost.id}</h1>
            <div>
-                <p>
-              <strong className="PostCommentStrong">Post-ID:</strong> {props.InfoPost.id}
-            </p>
               <p>
               <strong className="PostCommentStrong">Post-title:</strong> {props.InfoPost.title}
             </p>
@@ -22,14 +19,15 @@ const ShowData = (props) =>{
             </div>
             <div>
             <p>
+              <strong className="PostCommentStrong">Comment-Text:</strong> {props.InfoComment.body}
+            </p>
+            <p>
               <strong className="PostCommentStrong">Name:</strong> {props.InfoComment.name}
             </p>
             <p>
               <strong className="PostCommentStrong">E-mail:</strong> {props.InfoComment.email}
             </p>
-            <p>
-              <strong className="PostCommentStrong">Comment-Text:</strong> {props.InfoComment.body}
-            </p>
+            
             </div>
            <Link className="button" to="/">
            <button className="BTN">Tillbaka till startsidan</button>
